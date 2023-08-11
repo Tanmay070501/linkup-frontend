46e4c865d74e7e0d5fac69ff0a7c811f3091bafd
Tanmay Maheshwari <xyzabc.am89@gmail.com>
2023-08-11 15:31:35 +0530

    improved implementation of common components with error handling too

--- src/components/common/DarkButton.tsx
+++ src/components/common/DarkButton.tsx
@@ -1,13 +1,17 @@
 import React, { ReactNode } from 'react'
+import Loader from "../../assets/bars.svg"
 interface IDarkButton {
     children: ReactNode,
     onClick?: React.MouseEventHandler<HTMLButtonElement>,
-    className?: string
+    className?: string,
+    loading?: boolean,
+    disabled?: boolean,
 }
-function DarkButton({children, onClick, className}:IDarkButton) {
+function DarkButton({children, onClick, className, loading, disabled}:IDarkButton) {
   return (
-    <button className={`bg-[#623dff] p-3 rounded-lg text-white ${className}`} onClick={onClick}>
-        {children}
+    <button disabled={disabled || loading} className={`bg-[#623dff] p-3 rounded-lg text-white ${className} hover:opacity-90 disabled:opacity-75`} onClick={onClick}>
+        {!loading && children}
+        {loading && <img src={Loader} className='pointer-events-none w-5 m-auto'/>}
     </button>
   )
 }
--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { ForwardedRef } from "react";
 interface ITextInput {
     type?: React.HTMLInputTypeAttribute,
     placeholder?: string,
     className?: string,
     label?: string,
+    error?: string,
 }
-function TextInput({type, placeholder, className, label: labelText}: ITextInput) {
+const TextInput = React.forwardRef(({type, placeholder, label: labelText, error}: ITextInput, ref?: ForwardedRef<HTMLInputElement>) =>  {
   return (
     <div className="flex flex-col gap-2 w-full">
         <label className="text-xs font-light">{labelText}</label>
-        <input className="border rounded-lg p-2"  type={type} placeholder={placeholder} />
+        <div className="w-full relative">
+          <input ref={ref} className={`w-full border rounded-lg p-2 ${error && "border-red-400"}`}  type={type} placeholder={placeholder} />
+        {error && <p className="w-full text-red-400 font-thin text-sm text-right translate-y-full absolute bottom-0 left-0">{error}</p>}
+        </div>
     </div>
   )
-}
+})
 
 export default TextInput
